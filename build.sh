0b7cf39f7a485529975ad6cf39434f09a539aaa6
lekshmideepu <lekshmideepu@users.noreply.github.com>
2015-04-10 10:45:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -70,4 +70,7 @@ make
 make install
 make installcheck
 ls /home/travis/build/INM-6/nest-git-migration/build/reports
-cat /home/travis/build/INM-6/nest-git-migration/build/reports/TEST-core.phase_5.xml
+cat /home/travis/build/INM-6/nest-git-migration/build/reports/TEST-core.phase_2.xml
+if [ "$xMPI" = "MPI+" ] ; then
+  cat /home/travis/build/INM-6/nest-git-migration/build/reports/TEST-core.phase_5.xml
+fi  
