ada3fbf132711c7be0bdeab67f111774dc134c88
Alexander Peyser <a.peyser@fz-juelich.de>
2015-04-16 13:27:01 +0200

    check python

--- build.sh
+++ build.sh
@@ -9,44 +9,9 @@ cat > $HOME/.matplotlib/matplotlibrc <<EOF
     backend : svg
 EOF
 
-if [ "$xMPI" = "MPI+" ] ; then
-
-    # Fedora
-   # module load mpi/openmpi-i386
-   #openmpi
-   export LD_LIBRARY_PATH="/usr/lib/openmpi/lib:$LD_LIBRARY_PATH"
-   export CPATH="/usr/lib/openmpi/include:$CPATH"
-   export PATH="/usr/include/mpi:$PATH"
-   
-cat > $HOME/.nestrc <<EOF
-    % ZYV: NEST MPI configuration
-    /mpirun
-    [/integertype /stringtype]
-    [/numproc     /slifile]
-    {
-     () [
-      (mpirun -np ) numproc cvs ( ) statusdict/prefix :: (/bin/nest )  slifile
-     ] {join} Fold
-    } Function def
-EOF
- 
-    CONFIGURE_MPI="--with-mpi"
-
-else
     CONFIGURE_MPI="--without-mpi"
-fi
-
-if [ "$xPYTHON" = "Python+" ] ; then
     CONFIGURE_PYTHON="--with-python"
-else
-    CONFIGURE_PYTHON="--without-python"
-fi
-
-if [ "$xGSL" = "GSL+" ] ; then
-    CONFIGURE_GSL="--with-gsl"
-else
     CONFIGURE_GSL="--without-gsl"
-fi
 
 ./bootstrap.sh
 
